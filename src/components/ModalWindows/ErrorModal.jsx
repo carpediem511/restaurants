500a32c33f61ca259f0f370c401ec8e9db7cebf1
carpediem511 <vampire05@yandex.ru>
2024-02-24 17:16:30 +0300

    готова форма для банкета

--- src/components/ModalWindows/ErrorModal.jsx
+++ src/components/ModalWindows/ErrorModal.jsx
@@ -1,6 +1,62 @@
-const ErrorModal = () => {
+import { Dialog, Transition } from '@headlessui/react'
+import { Fragment } from 'react'
+
+const ErrorModal = ({ onClose, isOpen }) => {
 	return (
 		<>
+			<Transition appear show={isOpen} as={Fragment}>
+				<Dialog as="div" className="relative z-10" onClose={onClose}>
+					<Transition.Child
+						as={Fragment}
+						enter="ease-out duration-300"
+						enterFrom="opacity-0"
+						enterTo="opacity-100"
+						leave="ease-in duration-200"
+						leaveFrom="opacity-100"
+						leaveTo="opacity-0"
+					>
+						<div className="fixed inset-0 bg-black/25" />
+					</Transition.Child>
+
+					<div className="fixed inset-0 overflow-y-auto">
+						<div className="flex min-h-full items-center justify-center p-4 text-center">
+							<Transition.Child
+								as={Fragment}
+								enter="ease-out duration-300"
+								enterFrom="opacity-0 scale-95"
+								enterTo="opacity-100 scale-100"
+								leave="ease-in duration-200"
+								leaveFrom="opacity-100 scale-100"
+								leaveTo="opacity-0 scale-95"
+							>
+								<Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+									<Dialog.Title
+										as="h3"
+										className="text-2xl font-medium leading-6 text-red-900"
+									>
+										Ошибка отправки заказа!
+									</Dialog.Title>
+									<div className="mt-2">
+										<p className="text-lg text-red-500">
+											Пожалуйста, проверьте - корректно ли заполнены поля формы.
+
+										</p>
+									</div>
+
+									<div className="mt-4">
+										<button
+											type="button"
+											className="inline-flex justify-center rounded-md border border-transparent bg-red-100 px-4 py-2 text-lg font-medium text-red-900 hover:bg-red-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-red-500 focus-visible:ring-offset-2"
+											onClick={onClose}>
+											Сейчас
+										</button>
+									</div>
+								</Dialog.Panel>
+							</Transition.Child>
+						</div>
+					</div>
+				</Dialog>
+			</Transition>
 
 		</>
 	)
--- src/components/ModalWindows/index.jsx
+++ src/components/ModalWindows/index.jsx
@@ -1,9 +1,63 @@
-const SuccessModal = () => {
+import { Dialog, Transition } from '@headlessui/react'
+import { Fragment } from 'react'
+
+const SusccessModal = ({ onClose, isOpen }) => {
 	return (
 		<>
+			<Transition appear show={isOpen} as={Fragment}>
+				<Dialog as="div" className="relative z-10" onClose={onClose}>
+					<Transition.Child
+						as={Fragment}
+						enter="ease-out duration-300"
+						enterFrom="opacity-0"
+						enterTo="opacity-100"
+						leave="ease-in duration-200"
+						leaveFrom="opacity-100"
+						leaveTo="opacity-0"
+					>
+						<div className="fixed inset-0 bg-black/25" />
+					</Transition.Child>
 
+					<div className="fixed inset-0 overflow-y-auto">
+						<div className="flex min-h-full items-center justify-center p-4 text-center">
+							<Transition.Child
+								as={Fragment}
+								enter="ease-out duration-300"
+								enterFrom="opacity-0 scale-95"
+								enterTo="opacity-100 scale-100"
+								leave="ease-in duration-200"
+								leaveFrom="opacity-100 scale-100"
+								leaveTo="opacity-0 scale-95"
+							>
+								<Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+									<Dialog.Title
+										as="h3"
+										className="text-2xl font-medium leading-6 text-teal-900"
+									>
+										Ваша заявка принята!
+									</Dialog.Title>
+									<div className="mt-2">
+										<p className="text-lg text-teal-500">
+											Спасибо за доверие. Наш менеджер свяжется с Вами в ближайшее время.
+										</p>
+									</div>
+
+									<div className="mt-4">
+										<button
+											type="button"
+											className="inline-flex justify-center rounded-md border border-transparent bg-teal-100 px-4 py-2 text-lg font-medium text-teal-900 hover:bg-teal-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-teal-500 focus-visible:ring-offset-2"
+											onClick={onClose}>
+											Отлично!
+										</button>
+									</div>
+								</Dialog.Panel>
+							</Transition.Child>
+						</div>
+					</div>
+				</Dialog>
+			</Transition>
 		</>
 	)
 }
 
-export default SuccessModal
+export default SusccessModal
